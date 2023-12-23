cdcb74bfb944ca9229189f5a7861b30a1c6e27c0
Aleksandr Leonchenko <kram.kab@mail.ru>
2023-12-23 18:02:17 +0300

    commit_5 перед двумя парами ключей

--- test_3_project/simple_app_1/admin.py
+++ test_3_project/simple_app_1/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Item, Order, OrderItem
+from .models import Item, Order, OrderItem, Discount, Tax
 
 
 class ItemAdmin(admin.ModelAdmin):
@@ -33,9 +33,52 @@ class OrderItemAdmin(admin.ModelAdmin):
     ]
 
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'user',
+        'discount',
+        'tax',
+    ]
+    list_display_links = [
+        'id',
+        'user',
+        'discount',
+        'tax',
+    ]
+
+
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'amount',
+    ]
+    list_display_links = [
+        'id',
+        'name',
+        'amount',
+    ]
+
+
+class TaxAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'rate',
+    ]
+    list_display_links = [
+        'id',
+        'name',
+        'rate',
+    ]
+
+
 admin.site.register(Item, ItemAdmin)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(Discount, DiscountAdmin)
+admin.site.register(Tax, TaxAdmin)
 
 admin.site.site_title = 'Админ-панель test_3_project'
 admin.site.site_header = 'Админ-панель test_3_project'
--- test_3_project/simple_app_1/models.py
+++ test_3_project/simple_app_1/models.py
@@ -1,4 +1,6 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.conf import settings
 
 
 class Item(models.Model):
@@ -12,10 +14,44 @@ class Item(models.Model):
     )
 
 
+class Discount(models.Model):
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.amount}"
+
+
+class Tax(models.Model):
+    name = models.CharField(max_length=100)
+    rate = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
+
+    def __str__(self):
+        return f"{self.rate}%"
+
+
 class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
     items = models.ManyToManyField(
         Item,
         related_name='orders'
+    )
+    discount = models.ForeignKey(
+        Discount,
+        on_delete=models.SET_NULL,
+        null=True, blank=True
+    )
+    tax = models.ForeignKey(
+        Tax,
+        on_delete=models.SET_NULL,
+        null=True, blank=True
     )
 
     @property
@@ -23,6 +59,13 @@ class Order(models.Model):
         total = 0
         for order_item in OrderItem.objects.filter(order=self):
             total += order_item.item.price * order_item.quantity
+
+        if self.discount:
+            total -= self.discount.amount
+
+        if self.tax:
+            total += (total * self.tax.rate / 100)
+
         return total
 
 
